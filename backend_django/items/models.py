2c30fb76c90e2900d451b9db42e60dd28dc723ea
ondanje <cyrilondanje@gmail.com>
2024-02-14 22:21:15 +0300

    updated model

--- backend_django/items/models.py
+++ backend_django/items/models.py
@@ -2,47 +2,59 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description  = models.CharField(max_length=500)
-    category = models.CharField(max_length=255, default='default_category')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     location_found = models.CharField(max_length=50, blank=True, null=True)
     date_found = models.DateField()
     image = models.ImageField(upload_to='uploads/' ,default='add_item.png', max_length=100)
-    #owner = models.ForeignKey(get_user_model, on_delete=models.CASCADE, related_name='owned_items', null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_items', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 
-class Credential(Item):
-    type = models.CharField(max_length=255)
+# class Credential(Item):
+#     type = models.CharField(max_length=255)
 
-class Electronic(Item):
-    brand = models.CharField(max_length=255)
-    model = models.CharField(max_length=255)
+# class Electronic(Item):
+#     brand = models.CharField(max_length=255)
+#     model = models.CharField(max_length=255)
 
-class Clothing(Item):
-    pass
-    #size= models.CharField(max_length=255)
-    #color = models.CharField(max_length=255)
-
-
-class Pet(Item):
-    type = models.CharField(max_length=255)
+# class Clothing(Item):
+#     pass
+#     #size= models.CharField(max_length=255)
+#     #color = models.CharField(max_length=255)
 
 
-class Stationary(Item):
-    brand = models.CharField(max_length=255)
-    type = models.CharField(max_length=255)
+# class Pet(Item):
+#     type = models.CharField(max_length=255)
 
-    class Meta:
-        verbose_name_plural = "stationary"
 
-class Furniture(Item):
-    type = models.CharField(max_length=255)
+# class Stationary(Item):
+#     brand = models.CharField(max_length=255)
+#     type = models.CharField(max_length=255)
 
-    class Meta :
-        verbose_name_plural = "furniture"
+#     class Meta:
+#         verbose_name_plural = "stationary"
+
+# class Furniture(Item):
+#     type = models.CharField(max_length=255)
+
+#     class Meta :
+#         verbose_name_plural = "furniture"
