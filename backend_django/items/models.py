83e0496155354268b6c77095d0e936c3c563433f
ondanje <cyrilondanje@gmail.com>
2024-02-15 21:25:33 +0300

    items model

--- backend_django/items/models.py
+++ backend_django/items/models.py
@@ -23,6 +23,7 @@ class Item(models.Model):
     date_found = models.DateField()
     image = models.ImageField(upload_to='uploads/' ,default='add_item.png', max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_items', null=True, blank=True)
+    #contact = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
