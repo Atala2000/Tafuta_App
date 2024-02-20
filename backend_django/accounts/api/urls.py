d9ca41179211d88296dd95e9bdbc56d9dc82629f
ondanje <cyrilondanje@gmail.com>
2024-02-20 13:04:38 +0300

    fifth commit

--- backend_django/accounts/api/urls.py
+++ backend_django/accounts/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserLoginView, UserRegistrationView, UserLogoutView
+from .views import UserLoginView, UserRegistrationView, UserView, UserLogoutView
 
 urlpatterns = [
 	path('sign up/', UserRegistrationView.as_view(), name='user-registration'),
 	path ('login/', UserLoginView.as_view(), name='user-login'),
+	path ('user/', UserView.as_view()),
 	path ('logout/', UserLogoutView.as_view(), name='user-logout')
 ]
