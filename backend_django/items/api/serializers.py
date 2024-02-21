1c325b91da3c2b830501f1389d53dff511cab47b
ondanje <cyrilondanje@gmail.com>
2024-02-21 13:02:16 +0300

    removing owner

--- backend_django/items/api/serializers.py
+++ backend_django/items/api/serializers.py
@@ -1,36 +1,5 @@
 from rest_framework import serializers
 from ..models import Item, Category
-import logging
-
-# class ElectronicSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Electronic
-#         fields = '__all__'
-
-# class ClothingSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Clothing
-#         fields = '__all__'
-
-# class CredentialSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Credential
-#         fields = '__all__'
-
-# class PetSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Pet
-#         fields = '__all__'
-
-# class StationarySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Stationary
-#         fields = '__all__'
-
-# class FurnitureSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Furniture
-#         fields = '__all__'
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,11 +7,10 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ItemSerializer(serializers.ModelSerializer):
-    owner_username = serializers.ReadOnlyField(source='owner.username')
+    #owner_username = serializers.ReadOnlyField(source='owner.username')
     category_name = serializers.ReadOnlyField(source='category.name')
 
     class Meta:
         model = Item
-        fields = ['id', 'name', 'description', 'category', 'category_name', 'location_found', 'date_found', 'image', 'owner','owner_username']
-
+        fields = ['id', 'name', 'description', 'category', 'category_name', 'location_found', 'date_found', 'image']
 
--- backend_django/items/models.py
+++ backend_django/items/models.py
@@ -22,40 +22,8 @@ class Item(models.Model):
     location_found = models.CharField(max_length=50, blank=True, null=True)
     date_found = models.DateField()
     image = models.ImageField(upload_to='uploads/' ,default='add_item.png', max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_items', null=True, blank=True)
+    #owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_items', null=True, blank=True)
     #contact = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-
-
-# class Credential(Item):
-#     type = models.CharField(max_length=255)
-
-# class Electronic(Item):
-#     brand = models.CharField(max_length=255)
-#     model = models.CharField(max_length=255)
-
-# class Clothing(Item):
-#     pass
-#     #size= models.CharField(max_length=255)
-#     #color = models.CharField(max_length=255)
-
-
-# class Pet(Item):
-#     type = models.CharField(max_length=255)
-
-
-# class Stationary(Item):
-#     brand = models.CharField(max_length=255)
-#     type = models.CharField(max_length=255)
-
-#     class Meta:
-#         verbose_name_plural = "stationary"
-
-# class Furniture(Item):
-#     type = models.CharField(max_length=255)
-
-#     class Meta :
-#         verbose_name_plural = "furniture"
